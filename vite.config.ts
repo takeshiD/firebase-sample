8e255efedf688d3106f10dd5b8f4dc50633f315c
takeshid <goriponikeike55@gmail.com>
2025-08-22 10:51:45 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,36 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
-// import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 
 export default defineConfig({
     plugins: [
-        react(), 
-        tailwindcss(), 
-        // VitePWA()
+        react(),
+        tailwindcss(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicons/favicon.ico'],
+            injectRegister: 'auto',
+            manifest: {
+                name: 'Sample hello',
+                short_name: 'sha',
+                description: 'サンプル',
+                theme_color: '#eaeaea',
+                icons: [
+                    {
+                        src: 'favicons/icon-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'favicons/icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
     ],
     resolve: {
         extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
