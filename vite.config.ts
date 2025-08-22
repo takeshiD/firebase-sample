a5b64549818f10070c913ba6a0c647755f64a066
takeshid <goriponikeike55@gmail.com>
2025-08-22 10:07:29 +0900

    remove pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 
 export default defineConfig({
-    plugins: [react(), tailwindcss(), VitePWA()],
+    plugins: [
+        react(), 
+        tailwindcss(), 
+        // VitePWA()
+    ],
     resolve: {
         extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
         alias: {
